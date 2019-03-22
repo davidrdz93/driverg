bcf7c6772e723d669852d68c33fedaec35f59341
david rodriguez <davidsantiago.rp@gmail.com>
2019-03-23 00:03:26 +0100

    menu

--- src/driveg/DriveQuickStart.java
+++ src/driveg/DriveQuickStart.java
@@ -3,6 +3,7 @@ package driveg;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.List;
+import java.util.Scanner;
 
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.model.File;
@@ -12,11 +13,15 @@ public class DriveQuickStart
 {
 		
 	private static GoogleDriveService driveService = new GoogleDriveServiceImpl();
-		
-	public static void main(String[] args) throws IOException, GeneralSecurityException
-	{	
-		Drive service = driveService.getDrive();
-		
+	
+	private static void printMenu() 
+	{
+		System.out.println("1. Elenco files/cartelle");
+		System.out.println("9. Esci");
+	}
+	
+	private static void stampaElenco(Drive service) throws IOException
+	{
 		FileList result = service
 				.files()
 				.list()
@@ -33,8 +38,35 @@ public class DriveQuickStart
 			files.stream().forEach(file -> {
 				System.out.printf("Id: %s\t Nome: %s\n", file.getId(), file.getName());				
 			});
-		}
+		}		
+	}
 		
+	public static void main(String[] args) throws IOException, GeneralSecurityException
+	{	
+		Drive service = driveService.getDrive();
+
+		int scelta;
+		
+		Scanner scan = new Scanner(System.in);
+	
+		while (true)
+		{
+			printMenu();
+			scelta = scan.nextInt();			
+			
+			switch (scelta)
+			{
+				case 1:
+					stampaElenco(service);
+					break;
+				case 9:
+					return;
+				default:
+					System.out.println("Scelta errata");
+			
+			}	
+		
+		}	
 	}
 	
 }
